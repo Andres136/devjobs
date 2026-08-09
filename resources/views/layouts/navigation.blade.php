305339f7bfad93f962cd39f88e5081809aeb8d91
Elver581 <elveral100@gmail.com>
2026-08-08 21:31:57 -0500

    feat: agregar postulación a vacantes y control de acceso por roles

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -9,7 +9,8 @@
                         <x-application-logo class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200" />
                     </a>
                 </div>
-
+                 
+              @auth  
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
                     <x-nav-link :href="route('vacantes.index')" :active="request()->routeIs('vacantes.index')">
@@ -21,9 +22,14 @@
                     </x-nav-link>
                 </div>
             </div>
+            @endauth
+           
 
             <!-- Settings Dropdown -->
             <div class="hidden sm:flex sm:items-center sm:ms-6">
+                @auth
+                    
+              
                 <x-dropdown align="right" width="48">
                     <x-slot name="trigger">
                         <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150">
@@ -50,6 +56,20 @@
                         </form>
                     </x-slot>
                 </x-dropdown>
+             @endauth 
+                @guest  
+                <!-- Navigation Links -->
+                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                    <x-nav-link :href="route('login')" >
+                        {{ __('Iniciar Sesion') }}
+                    </x-nav-link>
+
+                    <x-nav-link :href="route('register')" >
+                        {{ __('Crear Cuenta') }}
+                    </x-nav-link>
+                </div>
+            </div>
+            @endguest     
             </div>
 
             <!-- Hamburger -->
@@ -66,6 +86,9 @@
 
     <!-- Responsive Navigation Menu -->
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
+       @auth
+           
+      
         <div class="pt-2 pb-3 space-y-1">
             <x-responsive-nav-link :href="route('vacantes.index')" :active="request()->routeIs('vacantes.index')">
                 {{ __('Mis vacantes') }}
@@ -95,5 +118,16 @@
                 </form>
             </div>
         </div>
+        @endauth
+        @guest
+              <div class="pt-2 pb-3 space-y-1">
+            <x-responsive-nav-link :href="route('login')" >
+                {{ __('Iniciar Sesion') }}
+            </x-responsive-nav-link>
+            <x-responsive-nav-link :href="route('register')" >
+                {{ __('Crear Cuenta') }}
+            </x-responsive-nav-link>
+        </div>
+        @endguest
     </div>
 </nav>
--- resources/views/livewire/mostrar-vacante.blade.php
+++ resources/views/livewire/mostrar-vacante.blade.php
@@ -30,4 +30,28 @@
             </p>
         </div>
     </div>
+
+    <div class="md:grid md:grid-cols-6 gap-4">
+        <div class="md:col-span-2">
+              <img src="{{ asset('storage/vacantes/'. $vacante->imagen) }}" alt="{{ 'Imagen vacante'. $vacante->titulo }}">
+        </div>
+
+        <div class="md:col-span-4">
+            <h2 class="text-2xl font-bold mb-5">Descripcion del Puesto</h2>
+             <p>{{ $vacante->descripcion }}</p>
+        </div>
+    </div>
+    @guest
+    <div class="mt-5 bg-gray-50  border-dashed p-5 text-center">
+        <p>
+            ¿Deseas aplicar a esta vacante? <a class="font-bold text-indigo-600" href="{{ route('register') }}">Obten una cuenta y aplica a esta y otras vacantes</a>
+        </p>
+    </div>
+    @endguest
+    @cannot('create', App\Models\Vacante::class)
+    <livewire:postular-vacante />
+    @endcannot
+   
+      
+    
 </div>
